f0c4a93cf1ed9967244f518a93fc66c390b3cef7
Handpear <handpear@outlook.com>
2023-09-08 14:52:44 +0800

    vue 兼容性修复

--- rules/vue/.eslintrc.js
+++ rules/vue/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     // https://github.com/mysticatea/vue-eslint-parser#-options
     parser: '@babel/eslint-parser',
     ecmaVersion: 2020,
+    extraFileExtensions: ['.vue'],
     // ECMAScript modules 模式
     sourceType: 'module',
     ecmaFeatures: {
